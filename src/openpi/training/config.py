12fe0a594614b67b5a404f6e93e79da15c6b2e1f
trd44 <tim.duggan93@gmail.com>
2025-09-04 12:17:30 -0400

    New training config for new dataset

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -785,6 +785,34 @@ _CONFIGS = [
         instruction_override="Play Towers of Hanoi.",
         num_workers=0,
     ),
+    TrainConfig(
+        name="pi0_hanoi_300_subtasks",
+        # Here is an example of loading a pi0 model for LoRA fine-tuning.
+        model=pi0.Pi0Config(
+            paligemma_variant="gemma_2b_lora",
+            action_expert_variant="gemma_300m_lora",
+            ignore_action_dims=(3, 4, 5),
+        ),
+        data=LeRobotLiberoDataConfig(
+            repo_id="tduggan93/hanoi_300_lerobot",
+            base_config=DataConfig(
+                prompt_from_task=True,
+            ),
+        ),
+        weight_loader=weight_loaders.CheckpointWeightLoader("s3://openpi-assets/checkpoints/pi0_base/params"),
+        num_train_steps=60_000,
+        # The freeze filter defines which parameters should be frozen during training.
+        # We have a convenience function in the model config that returns the default freeze filter
+        # for the given model config for LoRA finetuning. Just make sure it matches the model config
+        # you chose above.
+        freeze_filter=pi0.Pi0Config(
+            paligemma_variant="gemma_2b_lora", action_expert_variant="gemma_300m_lora"
+        ).get_freeze_filter(),
+        # Turn off EMA for LoRA finetuning.
+        ema_decay=None,
+        # instruction_override="Play Towers of Hanoi.",
+        # num_workers=0,
+    ),
 ]
 
 if len({config.name for config in _CONFIGS}) != len(_CONFIGS):
