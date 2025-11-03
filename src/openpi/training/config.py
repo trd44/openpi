f5b477cf99d1c8b1430ae8d40adce04175325a2f
mmcwilliams <Matthew.McWilliams@tufts.edu>
2025-11-03 17:50:40 -0500

    lora for finetuning paligemma and action expert

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -752,6 +752,36 @@ _CONFIGS = [
         pytorch_weight_path="/path/to/your/pytorch_weight_path",
         num_train_steps=30_000,
     ),
+    TrainConfig(
+        name="pi05_assembly_line_sorting",
+        model=pi0_config.Pi0Config(
+            pi05=True, 
+            action_dim=7,
+            action_horizon=10, 
+            discrete_state_input=False
+        ),
+        freeze_filter=pi0_config.Pi0Config(
+            paligemma_variant="gemma_2b_lora", action_expert_variant="gemma_300m_lora"
+        ).get_freeze_filter(),
+        
+        data=LeRobotLiberoDataConfig(
+            repo_id="tduggan93/assembly_line_sorting",
+            base_config=DataConfig(prompt_from_task=True),
+            extra_delta_transform=False,
+        ),
+        batch_size=256,
+        lr_schedule=_optimizer.CosineDecaySchedule(
+            warmup_steps=10_000,
+            peak_lr=5e-5,
+            decay_steps=1_000_000,
+            decay_lr=5e-5,
+        ),
+        optimizer=_optimizer.AdamW(clip_gradient_norm=1.0),
+        ema_decay=0.999,
+        weight_loader=weight_loaders.CheckpointWeightLoader("gs://openpi-assets/checkpoints/pi05_base/params"),
+        # pytorch_weight_path="/path/to/your/pytorch_weight_path",
+        num_train_steps=30_000,
+    ),
     #
     # Fine-tuning Aloha configs.
     #
