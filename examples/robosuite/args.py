b9eab13019716b98f3dbe27e7c71ed69d3aa27e0
pierrick.lorang@gmail.com <pierrick.lorang@gmail.com>
2026-02-10 11:09:38 -0500

    Updated to match ICRA settings

--- examples/robosuite/args.py
+++ examples/robosuite/args.py
@@ -20,12 +20,12 @@ class Args:
     robots: str = "Panda"              # Robot model to use
     controller: str = "OSC_POSE"         # Robosuite controller name
     peg_xy_jitter: float = 0.0           # Hanoi specific; changes tower spawn location
-    cube_placement_noise: float = 0.025  # All other environments; Uniform noise in meters to add to cube x and y positions during spawn
+    cube_placement_noise: float = 0.01  # All other environments; Uniform noise in meters to add to cube x and y positions during spawn
     settle_steps: int = 50               # Number of steps to wait for objects to settle
-    horizon: int = 9050                  # Max steps per episode (includes settle_steps) 9050 = 5 minutes of video at 30fps with 50 settle steps
+    horizon: int = 7050                  # Max steps per episode (includes settle_steps) 9050 = 5 minutes of video at 30fps with 50 settle steps
 
     # --- Subtask Guidance ---
-    task_timeout: int = 600         # Number of steps to wait before timing out a task (600 = 20 seconds at 30 fps)
+    task_timeout: int = 750         # Number of steps to wait before timing out a task (600 = 20 seconds at 30 fps)
     planner_guided: bool = True    # True for planner-guided; False for end-to-end
     planner:str = "pddl"            # Planner to use: 'pddl' or 'gpt-5'
 
@@ -59,4 +59,4 @@ class Args:
 
     # --- Multi-configuration support --- Currently unused
     # random_block_placement: bool = False # Place blocks on pegs randomly according to Towers of Hanoi rules
-    random_block_selection: bool = True  # Randomly select 3 out of 4 blocks
+    random_block_selection: bool = False  # Randomly select 3 out of 4 blocks
